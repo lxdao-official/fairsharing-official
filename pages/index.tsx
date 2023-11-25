40edcda6bf3aca944f89e45a09840425386af020
KieSun <zx597813039@gmail.com>
2023-11-25 20:04:53 +0800

    feat: pizza

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Roboto, Lexend } from 'next/font/google';
 import Image from 'next/image';
 import { capitalizeFirstLetter } from '@/utils/utils';
 
-const roboto = Roboto({ weight: ['400', '500'], subsets: ['vietnamese'], display: 'swap' });
+const roboto = Roboto({ weight: ['400', '500'], subsets: ['latin'], display: 'swap' });
 const lexend = Lexend({
 	weight: ['400'],
 	subsets: ['latin'],
@@ -12,21 +12,39 @@ const lexend = Lexend({
 
 export default function Home() {
 	return (
-		<main className={`${roboto.className} ${lexend.variable} text-[16px]`}>
-			<header className="bg-black h-[110px] flex justify-between items-center text-white px-[90px] text-[20px]">
-				<span className="flex items-center gap-1">
-					<Image src="/logo.png" alt="logo" width={64} height={64} />
-					<span className="font-lexend">FairSharing</span>
-				</span>
-				<ul className="flex items-center gap-8">
-					{['twitter', 'telegram'].map((item) => (
-						<li className="flex items-center gap-2 cursor-pointer">
-							<Image src={`/${item}.png`} alt={item} width={28} height={28} />
-							<span>{capitalizeFirstLetter(item)}</span>
-						</li>
-					))}
-				</ul>
-			</header>
+		<main className={`${roboto} ${lexend.variable} text-[16px]`}>
+			<div className="bg-black">
+				<header className="sticky top-0 flex h-[110px] items-center justify-between px-[90px] text-base text-white">
+					<span className="flex items-center gap-1">
+						<Image src="/logo.png" alt="logo" width={64} height={64} />
+						<span className="font-lexend">FairSharing</span>
+					</span>
+					<ul className="flex items-center gap-8">
+						{['twitter', 'telegram'].map((item) => (
+							<li className="flex cursor-pointer items-center gap-2">
+								<Image src={`/${item}.png`} alt={item} width={28} height={28} />
+								<span>{capitalizeFirstLetter(item)}</span>
+							</li>
+						))}
+					</ul>
+				</header>
+				<div className="flex flex-col items-center pb-[181px] pt-[76px] text-white">
+					<Image src="/pizza.png" alt="pizza" width={719} height={248} />
+					<h1 className="mt-[41px] text-center font-lexend text-[90px] font-medium leading-[90px]">
+						Decentralize coordination,
+						<br />
+						power, and identity
+					</h1>
+					<p className="mt-[25px] text-center text-base leading-[20px] opacity-80">
+						Connect, collaborate, and contribute,
+						<br />
+						for community recognition, fair rewards, and non-custodial identity.
+					</p>
+					<button className="bg-gradient text-main mt-[20px] h-[56px] w-[180px] rounded text-base">
+						Launch App
+					</button>
+				</div>
+			</div>
 		</main>
 	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,17 @@ const config: Config = {
 		extend: {
 			backgroundImage: {
 				black: 'linear-gradient(180deg, #001E24 0%, #000811 100%);',
+				gradient: 'linear-gradient(281deg, #00C9FF -0.58%, #1DE9B6 47.72%, #92FE9D 97.04%);',
 			},
 			fontFamily: {
 				lexend: ['var(--font-lexend)'],
+				roboto: ['var(--font-roboto)'],
+			},
+			colors: {
+				main: '#0F172A',
+			},
+			fontSize: {
+				base: '20px',
 			},
 		},
 	},
